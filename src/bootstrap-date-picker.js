c20e764af72043d711fba4b1304030ee8a68ee78
MohamadMahdi-Tasha <98010276+MohamadMahdi-Tasha@users.noreply.github.com>
2023-01-29 20:17:23 +0330

    added some little js for a11y

--- src/bootstrap-date-picker.js
+++ src/bootstrap-date-picker.js
@@ -1,8 +1,16 @@
 // Codes By Mahdi Tasha
 const mtDatePickerToggler = document.querySelectorAll('.mt-date-picker-toggler');
+const mtDatePickerDayItem = document.querySelectorAll('.mt-date-picker-day-item');
+
+mtDatePickerDayItem.forEach(item => item.setAttribute('tabindex', '-1'))
 
 mtDatePickerToggler.forEach(toggler => {
     toggler.addEventListener('click', () => {
-        toggler.parentElement.toggleAttribute('data-opened')
+        const parentOfToggler = toggler.parentElement;
+        parentOfToggler.toggleAttribute('data-opened')
+
+        (parentOfToggler.getAttribute('data-opened') !== null)
+            ? mtDatePickerDayItem.forEach(item => item.setAttribute('tabindex', '1'))
+            : mtDatePickerDayItem.forEach(item => item.setAttribute('tabindex', '-1'))
     })
 })
