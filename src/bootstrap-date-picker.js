7b93f6dee527e2c37c64b374624a92bce0ffb4b7
MohamadMahdi-Tasha <98010276+MohamadMahdi-Tasha@users.noreply.github.com>
2023-01-29 20:14:05 +0330

    added js for opening date picker

--- src/bootstrap-date-picker.js
+++ src/bootstrap-date-picker.js
@@ -1,2 +1,8 @@
 // Codes By Mahdi Tasha
-// Variables
+const mtDatePickerToggler = document.querySelectorAll('.mt-date-picker-toggler');
+
+mtDatePickerToggler.forEach(toggler => {
+    toggler.addEventListener('click', () => {
+        toggler.parentElement.toggleAttribute('data-opened')
+    })
+})
