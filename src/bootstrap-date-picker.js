8c40d8e96fd4c8f680e7be3365cc338c68a218bf
MohamadMahdi-Tasha <98010276+MohamadMahdi-Tasha@users.noreply.github.com>
2023-01-29 18:40:15 +0330

    added script for selected day item

--- src/bootstrap-date-picker.js
+++ src/bootstrap-date-picker.js
@@ -2,9 +2,18 @@
 // Variables
 const datePickerToggler = document.querySelectorAll('.mt-date-picker-toggler');
 const buttonsInDataPicker = document.querySelectorAll('.mt-date-picker .mt-date-picker-main-side-grid button');
-const datePickerDayItem = document.querySelectorAll('.mt-date-picker-day-item:not(.mt-date-picker-today)');
+const datePickerDayItem = document.querySelectorAll('.mt-date-picker-main-side-grid .mt-date-picker-day-item:not(.mt-date-picker-today)');
 
 buttonsInDataPicker.forEach(button => button.setAttribute('tabindex', '-1'))
+
+datePickerDayItem.forEach(item => {
+    item.addEventListener('click', () => {
+        const selectedDayItem = document.querySelector('.mt-date-picker-main-side-grid .mt-date-picker-selected');
+
+        item.classList.add('mt-date-picker-selected');
+        selectedDayItem.classList.remove('mt-date-picker-selected')
+    })
+})
 
 datePickerToggler.forEach(toggler => {
     toggler.addEventListener('click', () => {
