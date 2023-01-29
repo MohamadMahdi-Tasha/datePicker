5c296e52ddddaeaa89cdb5b12dd098bea3620cd9
MohamadMahdi-Tasha <98010276+MohamadMahdi-Tasha@users.noreply.github.com>
2023-01-29 20:43:02 +0330

    fixed a bug . god damn

--- src/bootstrap-date-picker.js
+++ src/bootstrap-date-picker.js
@@ -1,16 +1,24 @@
 // Codes By Mahdi Tasha
 const mtDatePickerToggler = document.querySelectorAll('.mt-date-picker-toggler');
 const mtDatePickerDayItem = document.querySelectorAll('.mt-date-picker-day-item');
+const mtDatePickerDayItemWithoutTodayAndSelected = document.querySelectorAll('.mt-date-picker-day-item:not(.mt-date-picker-today, .mt-date-picker-selected)');
 
 mtDatePickerDayItem.forEach(item => item.setAttribute('tabindex', '-1'))
 
 mtDatePickerToggler.forEach(toggler => {
     toggler.addEventListener('click', () => {
         const parentOfToggler = toggler.parentElement;
-        parentOfToggler.toggleAttribute('data-opened')
+        const openedDatePickerHolder = document.querySelector('.mt-date-picker-holder[data-opened]');
+        const allMtDatePickerDayItem = parentOfToggler.querySelectorAll('.mt-date-picker-day-item');
 
-        (parentOfToggler.getAttribute('data-opened') !== null)
-            ? mtDatePickerDayItem.forEach(item => item.setAttribute('tabindex', '1'))
-            : mtDatePickerDayItem.forEach(item => item.setAttribute('tabindex', '-1'))
+        parentOfToggler.toggleAttribute('data-opened');
+
+        if (openedDatePickerHolder !== null) {
+            openedDatePickerHolder.removeAttribute('data-opened');
+        }
+
+        (parentOfToggler.getAttribute('data-opened') === null)
+            ? allMtDatePickerDayItem.forEach(item => item.setAttribute('tabindex', '-1'))
+            : allMtDatePickerDayItem.forEach(item => item.setAttribute('tabindex', '1'))
     })
 })
