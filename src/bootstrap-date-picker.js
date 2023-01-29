3ac34c3b86e80f5f38b1f065f0920bbee231cb73
MohamadMahdi-Tasha <98010276+MohamadMahdi-Tasha@users.noreply.github.com>
2023-01-29 18:37:24 +0330

    yea

--- src/bootstrap-date-picker.js
+++ src/bootstrap-date-picker.js
@@ -2,14 +2,18 @@
 // Variables
 const datePickerToggler = document.querySelectorAll('.mt-date-picker-toggler');
 const buttonsInDataPicker = document.querySelectorAll('.mt-date-picker .mt-date-picker-main-side-grid button');
+const datePickerDayItem = document.querySelectorAll('.mt-date-picker-day-item:not(.mt-date-picker-today)');
 
 buttonsInDataPicker.forEach(button => button.setAttribute('tabindex', '-1'))
 
 datePickerToggler.forEach(toggler => {
     toggler.addEventListener('click', () => {
-        const datePicker = toggler.parentElement;
+        const datePickerHolder = toggler.parentElement;
 
-        datePicker.toggleAttribute('data-opened');
-        buttonsInDataPicker.forEach(button => button.setAttribute('tabindex', '1'))
+        datePickerHolder.toggleAttribute('data-opened')
+        datePickerHolder.getAttribute('data-opened') === null
+            ? buttonsInDataPicker.forEach(button => button.setAttribute('tabindex', '-1'))
+            : buttonsInDataPicker.forEach(button => button.setAttribute('tabindex', '1'))
+
     })
 })
